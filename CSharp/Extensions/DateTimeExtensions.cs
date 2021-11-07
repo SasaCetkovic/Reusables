91579754556d5074255157eb9a6c108202a317aa
Saša Ćetković <sasac1001@gmail.com>
2021-11-07 03:41:52 +0100

    Add DateTimeExtensions

---
+++ CSharp/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,22 @@
+﻿namespace System
+{
+	public static class DateTimeExtensions
+	{
+        private static readonly DateTime _unixStartDateTime = new DateTime(1970, 1, 1);
+
+        public static long ToUnixTimestamp(this DateTime date)
+        {
+            return Convert.ToInt64(date.Subtract(_unixStartDateTime).TotalSeconds);
+        }
+
+        public static long ToUnixTimestamp(this DateTime? date)
+        {
+            if (!date.HasValue)
+            {
+                return 0;
+            }
+
+            return ToUnixTimestamp(date.Value);
+        }
+    }
+}
